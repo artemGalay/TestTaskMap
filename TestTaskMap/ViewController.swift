8b24e70a3c40064f36786cc7e70bd8c5c6f9d788
Artem Galay <iostema@mail.ru>
2022-09-29 22:54:45 +0300

    Add mapView.

--- TestTaskMap/ViewController.swift
+++ TestTaskMap/ViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
     }
 
+    private func setupHierarchy() {
+        view.addSubview(mapView)
 
+    }
+
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
 }
-
